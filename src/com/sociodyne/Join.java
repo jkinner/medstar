30fd896f55a254bc483f0e71d2b9c8b43c9242c1
Jason Kinner <jkinner@sociodyne.com>
2011-08-05 08:19:49 -0400

    Join utility class

---
+++ src/com/sociodyne/Join.java
@@ -0,0 +1,25 @@
+package com.sociodyne;
+
+
+public class Join {
+  private Join() {
+    // Utility class
+  }
+  
+  public static String join(String joiner, Iterable<String> parts) {
+    StringBuffer joinedStringBuffer = new StringBuffer();
+
+    if (parts.iterator().hasNext()) {
+      for (String part : parts) {
+        joinedStringBuffer.append(part);
+        joinedStringBuffer.append(joiner);
+      }
+
+      // Remove that extra joiner we put on there.
+      joinedStringBuffer.delete(joinedStringBuffer.length() - joiner.length(),
+          joinedStringBuffer.length());
+    }
+    
+    return joinedStringBuffer.toString();
+  }
+}
---
+++ test/com/sociodyne/JoinTest.java
@@ -0,0 +1,32 @@
+package com.sociodyne;
+
+import com.google.common.collect.ImmutableSet;
+
+import junit.framework.TestCase;
+
+
+public class JoinTest extends TestCase {
+  public void testEmptyList() throws Exception {
+    assertEquals("", Join.join(" ", ImmutableSet.<String>of()));
+  }
+
+  public void testOneItemList() throws Exception {
+    assertEquals("1", Join.join(" ", ImmutableSet.<String>of("1")));
+  }
+
+  public void testTwoItemList() throws Exception {
+    assertEquals("1 2", Join.join(" ", ImmutableSet.<String>of("1", "2")));
+  }
+
+  public void testComplexSeparator_withEmptyList() throws Exception {
+    assertEquals("", Join.join("', '", ImmutableSet.<String>of()));
+  }
+
+  public void testComplexSeparator_withOneItemList() throws Exception {
+    assertEquals("1", Join.join("', '", ImmutableSet.<String>of("1")));
+  }
+
+  public void testComplexSeparator_withTwoItemList() throws Exception {
+    assertEquals("1', '2", Join.join("', '", ImmutableSet.<String>of("1", "2")));
+  }
+}
